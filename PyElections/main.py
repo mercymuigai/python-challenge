1192879fa0437ddbb128f05b8b43d47d33eb0432
mercymuigai <muigai.mercy@gmail.com>
2020-02-22 12:31:42 -0600

    init

--- PyElections/main.py
+++ PyElections/main.py
@@ -1,10 +1,13 @@
-# Modules
+
 import os
 import csv
 from statistics import mean
+import collections
+import operator
 
 # Seting the  path for file
-csvpath ="houston_election_data.csv"
+csvpath = os.path.join('houston_election_data.csv')
+file_to_output = os.path.join('houston_election.csv')
 
 # Opening the CSV
 with open(csvpath, newline='', encoding='utf-8') as csv_file:
@@ -15,23 +18,37 @@ with open(csvpath, newline='', encoding='utf-8') as csv_file:
     total_Vote  = 0
     for row in reader:
         total_Vote += 1
-        
         if row[0] not in candidate.keys():
             candidate[row[0]] = 1
-             
-            
         else:
-           candidate[row[0]] += 1  
-           
-    for entry in candidate:
-        print(entry,"       : " ,(candidate[entry]/total_Vote)*100,"%", candidate[entry])
-   
-        
-  
- # print('Houston Mayoral Election Results')
-        
-        print(f'total Cast Vote : {total_Vote}')
-    
+           candidate[row[0]] += 1
+        candidate_sorted = sorted(candidate.items(), key=operator.itemgetter(1),reverse=True)
+
+    print()
+    print("--------------------------------------------------------")
+    print("Houston Mayoral Election Results")
+    print("--------------------------------------------------------")
+    print("Total Cast Votes: " + str('{:,}'.format(total_Vote)))
+    print("--------------------------------------------------------")
+    for r in candidate_sorted:
+        print(str(r[0]) + "  (" + str('{:,}'.format(r[1])) + ")" + " : " + str('{0:.2%}'.format((r[1])/total_Vote)) )
+    print("--------------------------------------------------------")
+    print ("1st Advancing Candidate: " + str(candidate_sorted[0]) + "\n" "2nd Advancing Candidate: "+ str(candidate_sorted[1]))
+    print("--------------------------------------------------------")
+
+# Output Files
+with open(file_to_output, 'w') as datafile:
+    writer = csv.writer(datafile)
+    writer.writerow(["Candidate Name", "Total Votes Received", "Percentage of Votes Received"])
+    for r in candidate_sorted:
+        #writer.writerow( str((r[0])) + str((r[1])) + str(((r[1])/total_Vote)))
+        writer.writerow(r)
+    writer.writerow(["1st Advancing Candidate :", str(candidate_sorted[0])])
+    writer.writerow(["2nd Advancing Candidate :", str(candidate_sorted[1])])
+
+
+ 
+
 
 
 
---
+++ PyFinances/main.py
@@ -0,0 +1,89 @@
+#Dependencies
+import csv
+import os
+
+
+
+# Files to Load
+file_to_load = os.path.join('budget_data.csv')
+file_to_output = os.path.join('budget_data.csv')
+
+
+# Variables to use
+total_months = 0
+total_revenue = 0
+
+prev_revenue = 0
+revenue_change = 0
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999999]
+
+revenue_changes = []
+
+# Reading of files
+with open(file_to_load) as revenue_data:
+    reader = csv.DictReader(revenue_data)
+
+   #making a loop
+    for row in reader:
+
+        # Calculating total_months
+        total_months = total_months + 1
+        total_revenue = total_revenue + int(row["Profit/Losses"])
+        # print(row)
+
+        # Calculations of average revenue changes
+        revenue_change = int(row["Profit/Losses"]) - prev_revenue
+
+        revenue_changes.append(int(row["Profit/Losses"])- prev_revenue)
+      
+        prev_revenue = int(row["Profit/Losses"])
+        
+
+        # Determining greatest increase
+        if (revenue_change > greatest_increase[1]):
+            greatest_increase[1] = revenue_change
+            greatest_increase[0] = row["Date"]
+
+        if (revenue_change < greatest_decrease[1]):
+            greatest_decrease[1] = revenue_change
+            greatest_decrease[0] = row["Date"]
+
+
+
+
+
+    
+    revenue_changes.pop(0)
+    revenue_avg = sum(revenue_changes) / (len(revenue_changes)-1)
+    
+   
+    
+
+    # Printing outputs
+    print()
+    print()
+    print()
+    print("Financial Analysis")
+    print("-------------------------")
+    print("Total Months: " + str(total_months))
+    print("Total Revenue: " + "$" + str(total_revenue))
+    print("Average Change: " + "$" + str(round(sum(revenue_changes) / len(revenue_changes),2)))
+    print("Greatest Increase: " + str(greatest_increase[0]) + " ($" +  str(greatest_increase[1]) + ")") 
+    print("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" +  str(greatest_decrease[1]) + ")")
+    
+
+
+
+
+# Output Files
+with open(file_to_output, "w") as txt_file:
+    txt_file.write("Total Months: " + str(total_months))
+    txt_file.write("\n")
+    txt_file.write("Total Revenue: " + "$" + str(total_revenue))
+    txt_file.write("\n")
+    txt_file.write("Average Change: " + "$" + str(round(sum(revenue_changes) / len(revenue_changes),2)))
+    txt_file.write("\n")
+    txt_file.write("Greatest Increase: " + str(greatest_increase[0]) + " ($" + str(greatest_increase[1]) + ")") 
+    txt_file.write("\n")
+    txt_file.write("Greatest Decrease: " + str(greatest_decrease[0]) + " ($" + str(greatest_decrease[1]) + ")")
